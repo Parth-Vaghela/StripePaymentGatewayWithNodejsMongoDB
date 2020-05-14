52e78b125b6915c951b944f6b065378e6601d021
parthvaghelahd <parthvaghelahd@gmail.com>
2020-05-14 15:43:09 +0530

    add comments to code

--- app.js
+++ app.js
@@ -6,15 +6,16 @@ const mongoose = require('mongoose');
 const router = require('./routes/route');
 const app = express();
 require('dotenv').config();
-
 const port = process.env.PORT || 3000;
 
+//static files setup
 app.use(express.static(path.join(__dirname, 'public')));
+//set a view engine
 app.set('view engine','ejs');
-
+//bodyParser Middelware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : false}));
-
+//mongoose connection
 mongoose.connect(process.env.mongoURL,{ 
   useNewUrlParser : true,
   useUnifiedTopology : true
@@ -25,10 +26,9 @@ mongoose.connect(process.env.mongoURL,{
     console.log('something went wrong with mongodb')
   }
 })
-
-
+//use router
 app.use('/', router);
-
+//listining on port * 3000
 app.listen(port, ()=> {
   console.log(`server starts on port * ${port}`)
 })
